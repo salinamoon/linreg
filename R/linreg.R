60f624f87ae70c2e06e457500e257f8a79b664b3
SalinaM <salinam@umich.edu>
2022-11-21 18:39:01 -0500

    created R function

---
+++ R/linreg.R
@@ -0,0 +1,16 @@
+#' @title Linear Regression
+#' @description Runs linear regression model
+#' @param y outcome or dependent variable
+#' @param x matrix of covariates or independent variables
+#' @param dataset dataset containing y and x
+#' @return a table of results
+#' @examples
+#' linreg(am, mpg, mtcars)
+#' @export
+
+linreg = function(y, x, dataset) {
+  Y = dataset[[substitute(y)]]
+  X = cbind(1, dataset[[substitute(x)]])
+  beta = solve(t(X)%*%X)%*%t(X)%*%Y
+  return(beta)
+}
