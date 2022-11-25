42761192759377d3416b8a7cde4ab53102f28b0f
SalinaM <salinam@umich.edu>
2022-11-25 11:08:13 -0500

    updated test and vignette

--- tests/testthat/test-linreg.R
+++ tests/testthat/test-linreg.R
@@ -1,3 +1,11 @@
-test_that("SLR works", {
-  expect_equal(linreg(rep(0,10), 1:10, mtcars), c(0, 0))
+test_that("linreg works", {
+  expect_equal(as.vector(as.matrix(linreg("hp",
+                                          list(cyl = "cyl"),
+                                          mtcars))),
+               as.vector(summary(lm(hp ~ cyl,
+                                    data = mtcars))$coefficients))
+  expect_equal(as.vector(as.matrix(linreg(mtcars$hp,
+                                          list(cyl = mtcars$cyl)))),
+               as.vector(summary(lm(hp ~ cyl,
+                                    data = mtcars))$coefficients))
 })
